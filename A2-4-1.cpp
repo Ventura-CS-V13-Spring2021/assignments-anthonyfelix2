7de699fc75bdb15eabbff44c3b160c6b8d69ec3a
AnthonyF23 <anthony_felix2@my.vcccd.edu>
2021-03-20 06:55:14 +0000

    2-4-1

---
+++ A2-4-1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+
+	int rdnum;
+	int N;
+	ofstream 	rdfile;
+
+	rdfile.open("rdnum.txt");
+
+	cout << "Enter the number of random nummbers to be generated \n";
+	cin >> N;
+	// rdfile << N << endl;
+	srand(time(0));
+	for(int i; i<N; i++)
+	{
+		rdnum = random() % 100;
+		rdfile << rdnum << endl;
+	}
+
+	rdfile.close();
+
+}
