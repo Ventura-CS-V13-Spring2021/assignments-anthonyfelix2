3a34a16a1fd5b0e158687612cd97a085e6e327aa
AnthonyF23 <anthony_felix2@my.vcccd.edu>
2021-03-21 23:36:46 +0000

    quiz2 Q1

---
+++ quiz2-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+// tried doing without creating a file but i cannot figure it out
+int main()
+{
+int rdnum0, rdnum1, rdnum2, rdnum3, rdnum4, rdnumN, rdnum;
+int N = 5;
+
+srand(time(0));
+	for(int i; i<N; i++)
+	{
+		rdnum = random() % 100;
+    rdnum0 = rdnum
+    rdnum1 = rdnum
+  }
+  
+}
---
+++ quiz2-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+
+}
