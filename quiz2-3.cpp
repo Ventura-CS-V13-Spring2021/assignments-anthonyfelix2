dcefb1f4244002fd3815d1817c0af8366b84c102
AnthonyF23 <anthony_felix2@my.vcccd.edu>
2021-03-22 00:45:32 +0000

    Question 3 is similar to one of our labs

--- quiz2-3.cpp
+++ quiz2-3.cpp
@@ -7,5 +7,28 @@ using namespace std;
 
 int main()
 {
+ int number1, number2, number3;
+   
+     cout << "Enter three integer values : " ;
+     cin >> number1 >> number2 >> number3 ;
+     
+     if (number1 == number2 && number1 == number3)
+     cout << "All the numbers you entered are duplicate_numbers";
 
+     else if (number2 == number1 && number2 == number3)
+     cout << "All the numbers you entered are duplicate_numbers";
+
+     else if(number1 == number2)
+     cout << "Two numbers you entered are the duplicate_numbers";
+
+     else if(number1 == number3)
+     cout << "Two numbers you entered are the duplicate_numbers";
+
+     else if(number2 == number3)
+     cout << "Two numbers you entered are the duplicate_numbers";
+
+     else
+     cout<< "None of the numbers you entered are duplicate_numbers";
+
+     return 0;
 }
