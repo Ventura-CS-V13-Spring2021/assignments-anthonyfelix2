1769e4b8e0ac97c776472dd00bf9bb9e1b18f376
AnthonyF23 <anthony_felix2@my.vcccd.edu>
2021-03-22 01:29:36 +0000

    almost done

--- quiz2-7.cpp
+++ quiz2-7.cpp
@@ -7,5 +7,22 @@ using namespace std;
 
 int main()
 {
+  int E_ID;
+  double salary;
+  string E_N, D_N;
+
+  ofstream file;
+  file.open("employee.txt");
+  
+  cout << "Please enter the employee ID, employee name, department name and salary. Please type in quit when done" << endl;
+  while(cin >> E_ID >> E_N >> D_N >> salary)
+  file << E_ID <<" " << E_N << " "<< D_N <<" " << salary;
+
+  file.close();
+
+  ifstream file1;
+  file1.open ("employee.txt");
+  
+
 
 }
