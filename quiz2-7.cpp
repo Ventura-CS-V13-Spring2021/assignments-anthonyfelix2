ee4a42a9a2cdfb114ae46d8d1c102bdbefd3aa16
AnthonyF23 <anthony_felix2@my.vcccd.edu>
2021-03-22 01:46:28 +0000

    best

--- quiz2-7.cpp
+++ quiz2-7.cpp
@@ -4,12 +4,13 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-
+// best i can 
 int main()
 {
   int E_ID;
   double salary;
   string E_N, D_N;
+  string " ";
 
   ofstream file;
   file.open("employee.txt");
@@ -22,7 +23,12 @@ int main()
 
   ifstream file1;
   file1.open ("employee.txt");
+
+  cout << "employee ID" << "employee name" << "department name" << "salary";
+  while(file1 >> E_ID >> " " >> E_N >> " " >> D_N >> " ">> salary)
+  cout << E_ID << E_N << D_N << salary;
   
+
 
 
 }
