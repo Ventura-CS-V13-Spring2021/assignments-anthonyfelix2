d9e3fbe13a53b3f5a5e3b5ce39d6659df64a6712
AnthonyF23 <anthony_felix2@my.vcccd.edu>
2021-03-22 00:00:05 +0000

    using files now

--- quiz2-1.cpp
+++ quiz2-1.cpp
@@ -7,15 +7,41 @@ using namespace std;
 // tried doing without creating a file but i cannot figure it out
 int main()
 {
-int rdnum0, rdnum1, rdnum2, rdnum3, rdnum4, rdnumN, rdnum;
-int N = 5;
+  int rdnum, max, P, S;
+  max = 0;
+	int N = 5;
+	ofstream 	rdmfile;
 
-srand(time(0));
+	rdmfile.open("rdnum.txt Q");
+  srand(time(0));
 	for(int i; i<N; i++)
 	{
 		rdnum = random() % 100;
-    rdnum0 = rdnum
-    rdnum1 = rdnum
+		rdmfile << rdnum << endl;
+	}
+
+	rdmfile.close();
+
+  ifstream 	rdmfile ;
+
+	rdmfile.open("rdnum.txt Q");
+
+  while( rdmfile  >> rdnum )
+  {
+    P++;
+if(max == 0)
+   {
+    max = rdnum;
+   }
+if (P>=1)
+{
+  if (rdnum>max)
+  S = rdnum;
+  else break;
+}
+cout << rdnum << endl;
   }
-  
+cout << S;
+
+rdmfile.close();
 }
