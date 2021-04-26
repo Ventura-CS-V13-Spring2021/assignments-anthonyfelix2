46671f1675795a6ad97abe1c2f42ea65e397753b
AnthonyF23 <>
2021-04-26 05:04:02 +0000

    .

--- Midterm/question2.cpp
+++ Midterm/question2.cpp
@@ -13,10 +13,18 @@ int main ()
 
 Int isGreater(int n);
 {
+  int prec;
+  if 
 
 }
 
 int getRdnum(void);
 {
-  
+  int rdum;
+  for (int i = 0; i < N, i++)
+  {
+    rdum = rand()%50
+    cout << "Random number: " << rdum << endl;
+    return rdum;
+  }
 }
