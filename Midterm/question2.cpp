39f9e9cceecd6b763f45ac14434424e0b010bf24
AnthonyF23 <>
2021-04-26 04:54:14 +0000

    ...

---
+++ Midterm/question2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int getRdnum(void);
+
+Int isGreater(int n);
+
+int main ()
+{
+  const int N = 10;
+  int
+}
+
+Int isGreater(int n);
+{
+
+}
+
+int getRdnum(void);
+{
+  
+}
