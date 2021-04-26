257034b2a975efc11a262884a954de0d10925cb7
AnthonyF23 <>
2021-04-26 04:16:22 +0000

    having trouble with something nearly done

--- Midterm/question1.cpp
+++ Midterm/question1.cpp
@@ -2,9 +2,9 @@
 #include <fstream>
 using namespace std;
 
-double sum(int a, int b)
+double sum(int , int )
 
-double avg(int a, int b)
+double avg(int , int )
 
 int main()
 {
@@ -19,6 +19,14 @@ int main()
         cerr << "File Open Error \n";
         exit(0);
     }
+
+  while(ifs >> name >> score1 >> score2)
+  {
+    cout << name "\t" << score1 "\t" << score2;
+    result1 = sum(score1, score2);
+    result2 = avg(score1, score2);
+    cout << "Sum : " << result1 << "Average: " << result2 << endl;
+  }
 
 }
 
